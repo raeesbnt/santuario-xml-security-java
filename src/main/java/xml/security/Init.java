ca8f00b1fe321050b049c90ed8ac9a50084b5aa8
raeesbnt <92310288+raeesbnt@users.noreply.github.com>
2024-10-24 17:26:03 +0200

    Add files via upload

---
+++ src/main/java/xml/security/Init.java
@@ -0,0 +1,328 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.regexbyte.pos.utils.generateSignature.xml.security;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.ArrayList;
+import java.util.List;
+
+import  com.regexbyte.pos.utils.generateSignature.xml.security.algorithms.JCEMapper;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.algorithms.SignatureAlgorithm;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.c14n.Canonicalizer;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.exceptions.XMLSecurityException;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.keys.keyresolver.KeyResolver;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.transforms.InvalidTransformException;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.transforms.Transform;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.utils.ClassLoaderUtils;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.utils.ElementProxy;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.utils.I18n;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.utils.XMLUtils;
+import  com.regexbyte.pos.utils.generateSignature.xml.security.utils.resolver.ResourceResolver;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+
+/**
+ * This class does the configuration of the library. This includes creating
+ * the mapping of Canonicalization and Transform algorithms. Initialization is
+ * done by calling {@link Init#init} which should be done in any static block
+ * of the files of this library. We ensure that this call is only executed once.
+ */
+public class Init {
+
+    /** The namespace for CONF file **/
+    public static final String CONF_NS = "http://www.xmlsecurity.org/NS/#configuration";
+
+
+
+    /** Field alreadyInitialized */
+    private static boolean alreadyInitialized = false;
+
+    /**
+     * Method isInitialized
+     * @return true if the library is already initialized.
+     */
+    public static final synchronized boolean isInitialized() {
+        return Init.alreadyInitialized;
+    }
+
+    /**
+     * Method init
+     *
+     */
+    public static synchronized void init() {
+        if (alreadyInitialized) {
+            return;
+        }
+        PrivilegedAction<InputStream> action = () -> {
+            String cfile = System.getProperty("com.regexbyte.pos.utils.generateSignature.xml.security.resource.config");
+            if (cfile == null) {
+                return null;
+            }
+            return ClassLoaderUtils.getResourceAsStream(cfile, Init.class);
+        };
+        try (InputStream is = AccessController.doPrivileged(action)) {
+            if (is == null) {
+                dynamicInit();
+            } else {
+                fileInit(is);
+            }
+        } catch (IOException ex) {
+            Log.e("warning", ex.getMessage());
+
+        }
+
+        alreadyInitialized = true;
+    }
+
+    /**
+     * Dynamically initialise the library by registering the default algorithms/implementations
+     */
+    private static void dynamicInit() {
+        //
+        // Load the Resource Bundle - the default is the English resource bundle.
+        // To load another resource bundle, call I18n.init(...) before calling this
+        // method.
+        //
+        I18n.init("en", "US");
+        Log.e("debug","Registering default algorithms");
+
+        try {
+            //
+            // Bind the default prefixes
+            //
+            ElementProxy.registerDefaultPrefixes();
+        } catch (XMLSecurityException ex) {
+            Log.e("error", ex.getMessage());
+        }
+
+        //
+        // Set the default Transforms
+        //
+        Transform.registerDefaultAlgorithms();
+
+        //
+        // Set the default signature algorithms
+        //
+        SignatureAlgorithm.registerDefaultAlgorithms();
+
+        //
+        // Set the default JCE algorithms
+        //
+        JCEMapper.registerDefaultAlgorithms();
+
+        //
+        // Set the default c14n algorithms
+        //
+        Canonicalizer.registerDefaultAlgorithms();
+
+        //
+        // Register the default resolvers
+        //
+        ResourceResolver.registerDefaultResolvers();
+
+        //
+        // Register the default key resolvers
+        //
+        KeyResolver.registerDefaultResolvers();
+    }
+
+    /**
+     * Initialise the library from a configuration file
+     */
+    private static void fileInit(InputStream is) {
+        try {
+            /* read library configuration file */
+            Document doc = XMLUtils.read(is, true);
+            Node config = doc.getFirstChild();
+            for (; config != null; config = config.getNextSibling()) {
+                if ("Configuration".equals(config.getLocalName())) {
+                    break;
+                }
+            }
+            if (config == null) {
+                Log.e("warning", "Error in reading configuration file - Configuration element not found");
+
+                return;
+            }
+            for (Node el = config.getFirstChild(); el != null; el = el.getNextSibling()) {
+                if (Node.ELEMENT_NODE != el.getNodeType()) {
+                    continue;
+                }
+                String tag = el.getLocalName();
+                if ("ResourceBundles".equals(tag)) {
+                    Element resource = (Element)el;
+                    /* configure internationalization */
+                    Attr langAttr = resource.getAttributeNodeNS(null, "defaultLanguageCode");
+                    Attr countryAttr = resource.getAttributeNodeNS(null, "defaultCountryCode");
+                    String languageCode =
+                        (langAttr == null) ? null : langAttr.getNodeValue();
+                    String countryCode =
+                        (countryAttr == null) ? null : countryAttr.getNodeValue();
+                    I18n.init(languageCode, countryCode);
+                }
+
+                if ("CanonicalizationMethods".equals(tag)) {
+                    Element[] list =
+                        XMLUtils.selectNodes(el.getFirstChild(), CONF_NS, "CanonicalizationMethod");
+
+                    for (Element element : list) {
+                        String uri = element.getAttributeNS(null, "URI");
+                        String javaClass =
+                            element.getAttributeNS(null, "JAVACLASS");
+                        try {
+                            Canonicalizer.register(uri, javaClass);
+                            Log.e("debug", "Canonicalizer.register({0}, {1})");
+
+                        } catch (ClassNotFoundException e) {
+                            Object[] exArgs = { uri, javaClass };
+                            Log.e("error", I18n.translate("algorithm.classDoesNotExist"));
+
+                        }
+                    }
+                }
+
+                if ("TransformAlgorithms".equals(tag)) {
+                    Element[] tranElem =
+                        XMLUtils.selectNodes(el.getFirstChild(), CONF_NS, "TransformAlgorithm");
+
+                    for (Element element : tranElem) {
+                        String uri = element.getAttributeNS(null, "URI");
+                        String javaClass =
+                            element.getAttributeNS(null, "JAVACLASS");
+                        try {
+                            Transform.register(uri, javaClass);
+                            Log.d("debug", "Transform.register({0}, {1})");
+                         //   LOG.log(Level.DEBUG, "Transform.register({0}, {1})", uri, javaClass);
+                        } catch (ClassNotFoundException e) {
+                            Object[] exArgs = { uri, javaClass };
+
+                            Log.e("ERROR", I18n.translate("algorithm.classDoesNotExist", exArgs));
+                        } catch (NoClassDefFoundError ex) {
+                            Log.w("WARNING", "Not able to found dependencies for algorithm, I'll keep working.");
+                          //  LOG.log(Level.WARNING, "Not able to found dependencies for algorithm, I'll keep working.");
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+
+                if ("JCEAlgorithmMappings".equals(tag)) {
+                    Node algorithmsNode = ((Element)el).getElementsByTagName("Algorithms").item(0);
+                    if (algorithmsNode != null) {
+                        Element[] algorithms =
+                            XMLUtils.selectNodes(algorithmsNode.getFirstChild(), CONF_NS, "Algorithm");
+                        for (Element element : algorithms) {
+                            String id = element.getAttributeNS(null, "URI");
+                            JCEMapper.register(id, new JCEMapper.Algorithm(element));
+                        }
+                    }
+                }
+
+                if ("SignatureAlgorithms".equals(tag)) {
+                    Element[] sigElems =
+                        XMLUtils.selectNodes(el.getFirstChild(), CONF_NS, "SignatureAlgorithm");
+
+                    for (Element sigElem : sigElems) {
+                        String uri = sigElem.getAttributeNS(null, "URI");
+                        String javaClass =
+                            sigElem.getAttributeNS(null, "JAVACLASS");
+
+                        /** $todo$ handle registering */
+
+                        try {
+                            SignatureAlgorithm.register(uri, javaClass);
+                            Log.d("DEBUG", "SignatureAlgorithm.register({0}, {1}) "+ uri+" "+ javaClass);
+                        } catch (ClassNotFoundException e) {
+                            Object[] exArgs = { uri, javaClass };
+                            Log.e("ERROR", I18n.translate("algorithm.classDoesNotExist", exArgs));
+                        }
+                    }
+                }
+
+                if ("ResourceResolvers".equals(tag)) {
+                    Element[] resolverElem =
+                        XMLUtils.selectNodes(el.getFirstChild(), CONF_NS, "Resolver");
+                    List<String> classNames = new ArrayList<>(resolverElem.length);
+                    for (Element element : resolverElem) {
+                        String javaClass =
+                            element.getAttributeNS(null, "JAVACLASS");
+                        String description =
+                            element.getAttributeNS(null, "DESCRIPTION");
+
+                        if (description != null && description.length() > 0) {
+                            Log.d("DEBUG", "Register Resolver: {0}: {1}"+" "+ javaClass+" "+ description);
+                        } else {
+                            Log.d("DEBUG", "Register Resolver: {0}: For unknown purposes " + javaClass);
+                        }
+                        classNames.add(javaClass);
+                    }
+                    ResourceResolver.registerClassNames(classNames);
+                }
+
+                if ("KeyResolver".equals(tag)){
+                    Element[] resolverElem =
+                        XMLUtils.selectNodes(el.getFirstChild(), CONF_NS, "Resolver");
+                    List<String> classNames = new ArrayList<>(resolverElem.length);
+                    for (Element element : resolverElem) {
+                        String javaClass =
+                            element.getAttributeNS(null, "JAVACLASS");
+                        String description =
+                            element.getAttributeNS(null, "DESCRIPTION");
+
+                        if (description != null && description.length() > 0) {
+                            Log.d("DEBUG", "Register Resolver: {0}: {1} "+ javaClass+" "+ description);
+                        } else {
+                            Log.d("DEBUG", "Register Resolver: {0}: For unknown purposes "+ javaClass);
+                        }
+                        classNames.add(javaClass);
+                    }
+                    KeyResolver.registerClassNames(classNames);
+                }
+
+
+                if ("PrefixMappings".equals(tag)){
+                    Log.d("DEBUG", "Now I try to bind prefixes:");
+
+                    Element[] nl =
+                        XMLUtils.selectNodes(el.getFirstChild(), CONF_NS, "PrefixMapping");
+
+                    for (Element element : nl) {
+                        String namespace = element.getAttributeNS(null, "namespace");
+                        String prefix = element.getAttributeNS(null, "prefix");
+                     //   LOG.log(Level.DEBUG, "Now I try to bind {0} to {1}", prefix, namespace);
+                        ElementProxy.setDefaultPrefix(namespace, prefix);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e("ERROR", "Bad: ", e);
+        }
+    }
+
+}
+
---
+++ src/main/java/xml/security/stax/ext/ComparableType.java
@@ -0,0 +1,72 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.regexbyte.pos.utils.generateSignature.xml.security.stax.ext;
+
+import java.util.Objects;
+
+/**
+ * Two comparable types are compared by their name.
+ *
+ * @param <T> Compatible class type.
+*/
+public abstract class ComparableType<T extends ComparableType<T>> implements Comparable<T> {
+
+    private final String name;
+
+    public ComparableType(String name) {
+        this.name = Objects.requireNonNull(name, "name");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        if (this.getClass().isAssignableFrom(obj.getClass())) {
+            @SuppressWarnings("unchecked")
+            T other = (T) obj;
+            if (this.getName().equals(other.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public int compareTo(T o) {
+        return this.getName().compareTo(o.getName());
+    }
+}
