141991b062c6f2f96c1e5d504178c96a8a0f9c91
raeesbnt <92310288+raeesbnt@users.noreply.github.com>
2024-10-24 17:23:18 +0200

    Add files via upload

---
+++ src/main/java/xml/security/encryption/XMLCipherUtil.java
@@ -0,0 +1,83 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.regexbyte.pos.utils.generateSignature.xml.security.encryption;
+
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.security.spec.AlgorithmParameterSpec;
+
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+
+import  com.regexbyte.pos.utils.generateSignature.xml.security.utils.EncryptionConstants;
+
+public final class XMLCipherUtil {
+
+  //  private static final Logger LOG = System.getLogger(XMLCipherUtil.class.getName());
+
+    private static final boolean gcmUseIvParameterSpec =
+        AccessController.doPrivileged((PrivilegedAction<Boolean>)
+            () -> Boolean.getBoolean("com.regexbyte.pos.utils.generateSignature.xml.security.cipher.gcm.useIvParameterSpec"));
+
+    /**
+     * Build an <code>AlgorithmParameterSpec</code> instance used to initialize a <code>Cipher</code> instance
+     * for block cipher encryption and decryption.
+     *
+     * @param algorithm the XML encryption algorithm URI
+     * @param iv the initialization vector
+     * @return the newly constructed AlgorithmParameterSpec instance, appropriate for the
+     *         specified algorithm
+     */
+    public static AlgorithmParameterSpec constructBlockCipherParameters(String algorithm, byte[] iv) {
+        if (EncryptionConstants.ALGO_ID_BLOCKCIPHER_AES128_GCM.equals(algorithm)
+                || EncryptionConstants.ALGO_ID_BLOCKCIPHER_AES192_GCM.equals(algorithm)
+                || EncryptionConstants.ALGO_ID_BLOCKCIPHER_AES256_GCM.equals(algorithm)) {
+            return constructBlockCipherParametersForGCMAlgorithm(algorithm, iv);
+        } else {
+         //   LOG.log(Level.DEBUG, "Saw non-AES-GCM mode block cipher, returning IvParameterSpec: {0}", algorithm);
+            return new IvParameterSpec(iv);
+        }
+    }
+
+    public static AlgorithmParameterSpec constructBlockCipherParameters(boolean gcmAlgorithm, byte[] iv) {
+        if (gcmAlgorithm) {
+            return constructBlockCipherParametersForGCMAlgorithm("AES/GCM/NoPadding", iv);
+        } else {
+            //LOG.log(Level.DEBUG, "Saw non-AES-GCM mode block cipher, returning IvParameterSpec");
+            return new IvParameterSpec(iv);
+        }
+    }
+
+    private static AlgorithmParameterSpec constructBlockCipherParametersForGCMAlgorithm(String algorithm, byte[] iv) {
+        if (gcmUseIvParameterSpec) {
+            // This override allows to support Java 1.7+ with (usually older versions of) third-party security
+            // providers which support or even require GCM via IvParameterSpec rather than GCMParameterSpec,
+            // e.g. BouncyCastle <= 1.49 (really <= 1.50 due to a semi-related bug).
+            //LOG.log(Level.DEBUG, "Saw AES-GCM block cipher, using IvParameterSpec due to system property override: {0}", algorithm);
+            return new IvParameterSpec(iv);
+        }
+
+       // LOG.log(Level.DEBUG, "Saw AES-GCM block cipher, attempting to create GCMParameterSpec: {0}", algorithm);
+
+        GCMParameterSpec gcmSpec = new GCMParameterSpec(128, iv);
+       // LOG.log(Level.DEBUG, "Successfully created GCMParameterSpec");
+        return gcmSpec;
+    }
+}
